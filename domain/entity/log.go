5c31f0df6b33fc4da9eb9ff9806651c74caf5dc6
mahdi mehrabi <mahdimehrabi17@gmail.com>
2024-05-30 03:05:37 +0330

    feat: log constructor

--- domain/entity/log.go
+++ domain/entity/log.go
@@ -1,6 +1,15 @@
 package entity
 
+import "time"
+
 type Log struct {
 	CreatedAt uint64 `json:"createdAt"`
 	Error     string `json:"error"`
+}
+
+func NewLog(err string) *Log {
+	return &Log{
+		Error:     err,
+		CreatedAt: uint64(time.Now().Unix()),
+	}
 }
